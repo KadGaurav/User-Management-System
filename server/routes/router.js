0cc5d0402469b8c1d2bef70e4e5fcd27baa8ee0f
KadGaurav <gauravkad139@gmail.com>
2022-09-07 22:48:39 +0530

    Client Side Ready

---
+++ server/routes/router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.render('index');
+});
+
+router.get("/add-user",(req,res)=>{
+    res.render('add_user');
+});
+
+router.get("/update-user",(req,res)=>{
+    res.render('update_user');
+});
